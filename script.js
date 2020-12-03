ae04fc3013a36c611eefa62fa462fef764c29944
Deividas <deivis124@gmail.com>
2020-12-03 06:19:05 +0200

    Adds Infinity Scroll Functionality

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let = totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "gkXQzvLHE6aInvSTVJ0dvbNNmhZvMnSM0mCj21CaSjg";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  console.log("image loaded");
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log("ready = ", ready);
+  }
+}
 
 // Helper Function to set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,6 +32,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("total images", totalImages);
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -32,6 +50,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -48,6 +68,33 @@ async function getPhotos() {
     // Catch Error Here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    // Total height of browser window
+    console.log("window.innerHeight =" + window.innerHeight);
+    // Distance from top of page user has scrolled
+    console.log("window.scrollY =" + window.scrollY);
+    console.log(
+      "window.innerHeight + scrollY =",
+      window.innerHeight + window.scrollY
+    );
+    // Height of everything in the body, including what is not within view
+    console.log("document.body.offsetHeight =" + document.body.offsetHeight);
+    console.log(
+      "document.body.offsetHeight-1000 =",
+      document.body.offsetHeight - 1000
+    );
+
+    ready = false;
+    getPhotos();
+    console.log("load more");
+  }
+});
 
 // On Load
 getPhotos();
